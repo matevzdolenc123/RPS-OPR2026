f7560294cdcd59498c94b3e72681dae4be35587e
matevzdolenc123 <matevz.dolenc123@gmail.com>
2026-09-11 09:19:38 +0000

    poprvek

--- VAJE/openMeteo.py
+++ VAJE/openMeteo.py
@@ -1,6 +1,7 @@
 import requests
 def trenutna_temp(lat, long):
-    base_url=f"https://api.open-meteo.com/v1/forecast?latitude=(lat)&longitude=(long)&current=temperature_2m&timezone=auto&forecast_days=1"
+    base_url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current=temperature_2m,weather_code&timezone=auto"
     call= requests.get(base_url).json()
     print(call["current"]["temperature_2m"])
 trenutna_temp(45.12, 14.5)
+
